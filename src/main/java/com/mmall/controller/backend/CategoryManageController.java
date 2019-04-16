98ab949a9867e4840353c6420fa7d700661998f7
hc1379321759 <1379321759@qq.com>
2019-04-16 18:26:39 +0800

    后台商品管理模块完成

--- src/main/java/com/mmall/controller/backend/CategoryManageController.java
+++ src/main/java/com/mmall/controller/backend/CategoryManageController.java
@@ -3,7 +3,6 @@ package com.mmall.controller.backend;
 import com.mmall.common.Const;
 import com.mmall.common.ResponseCode;
 import com.mmall.common.ServerResponse;
-import com.mmall.pojo.Category;
 import com.mmall.pojo.User;
 import com.mmall.service.ICategoryService;
 import com.mmall.service.IUserService;
--- src/main/java/com/mmall/service/impl/CategoryServiceImpl.java
+++ src/main/java/com/mmall/service/impl/CategoryServiceImpl.java
@@ -19,9 +19,9 @@ import java.util.Set;
 
 
 @Service("iCategoryService")
-public class ICategoryServiceImpl implements ICategoryService {
+public class CategoryServiceImpl implements ICategoryService {
 
-    private Logger logger = LoggerFactory.getLogger(ICategoryServiceImpl.class);
+    private Logger logger = LoggerFactory.getLogger(CategoryServiceImpl.class);
 
     @Autowired
     private CategoryMapper categoryMapper;
